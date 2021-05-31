a95c342dfc3ef8fc397ce343a1037617f2b092ad
JaviMera <javier_mera88@hotmail.com>
2021-05-31 10:35:55 -0500

    Added employee model and changed index page UI to show employee and dependent input controls.

---
+++ PayrollCalculator.Web/Models/EmployeeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PayrollCalculator.Web.Models
+{
+    public sealed class EmployeeModel
+    {
+        public string Name { get; set; }
+        public string SpouseName { get; set; }
+        public string ChildOne { get; set; }
+        public string ChildTwo { get; set; }
+    }
+}
