dc7c87d6d69587439bc68f73bf3c24fe73cb1654
JaviMera <javier_mera88@hotmail.com>
2021-05-31 15:29:01 -0500

    Removed unused properties.

--- PayrollCalculator.Web/Models/EmployeeModel.cs
+++ PayrollCalculator.Web/Models/EmployeeModel.cs
@@ -5,9 +5,6 @@ namespace PayrollCalculator.Web.Models
     public sealed class EmployeeModel
     {
         public string Name { get; set; }
-        public List<DependentModel> Dependents { get; set; } = new List<DependentModel>();
-        public string SpouseName { get; set; }
-        public string ChildOne { get; set; }
-        public string ChildTwo { get; set; }
+        public List<DependentModel> Dependents { get; set; } = new List<DependentModel>();        
     }
 }
